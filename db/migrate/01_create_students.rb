7cca410559736380fe4207b20b4bde69f4e61cf4
JazzyMussels <jeffmussels@gmail.com>
2019-09-05 13:20:38 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    create_table :students do |t|
-      t.string :name
+    create_table :students, :name, :string
     end
   end
 end
